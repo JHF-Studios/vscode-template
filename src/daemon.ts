5c2d20acf08bb55b4971ae0d91a5ba45ebd1975e
edunbar <edunbar@unca.edu>
2023-11-30 13:37:50 -0500

    updated save and scripts

--- src/daemon.ts
+++ src/daemon.ts
@@ -33,6 +33,7 @@ export async function main(ns:NS) {
         }
         
     }
+
     //set minSec to targets lowest security
     const MINSEC = ns.getServerMinSecurityLevel(target);
 
--- src/testing.ts
+++ src/testing.ts
@@ -2,4 +2,4 @@ import { NS } from '@ns'
 
 export async function main(ns : NS) : Promise<void> {
     ns.exec('hack.js', 'home', 1, "--target", "foodnstuff", "--persist", true );
-}
+} 
