f469b9d5cd20e6af7333177a0d7e0f2db796bf5a
Mark P <mp5093@gmail.com>
2023-11-29 21:43:26 -0500

    killall updated

--- src/killall.ts
+++ src/killall.ts
@@ -4,4 +4,6 @@ export async function main(ns : NS) : Promise<void> {
     //
     const servers : string[] = ns.getPurchasedServers();
     
+    for (let i = 0; i < servers.length; i++) {
+        ns.killall(servers[i]);
 }
