7ac7a55573feabbfe14522143848b5326db1ce34
Mark P <mp5093@gmail.com>
2023-11-29 22:27:17 -0500

    update serverstats

---
+++ src/master.ts
@@ -0,0 +1,9 @@
+import { NS } from '@ns'
+
+export async function main(ns : NS) : Promise<void> {
+    //
+    const servers : string[] = ns.getPurchasedServers();
+    const player = ns.getPlayer();
+    const server =    
+
+}
--- src/serverstats.ts
+++ src/serverstats.ts
@@ -3,9 +3,17 @@ import { NS } from '@ns'
 export async function main(ns : NS) : Promise<void> {
     //
     const servers: Array<string> = ns.getPurchasedServers();
-    
+    const player = ns.getPlayer();
+
     for (let i = 0; i < servers.length; i++) {
-    const server = ns.getServer(servers[i]);
-    ns.tprint("Hostname: " + server.hostname, " MaxRAM: " + server.maxRam)
+        const server = ns.getServer(servers[i]);
+        const maxRAM = ns.getPurchasedServerMaxRam(servers[i]);
+        const ramUpgrade = ns.getPurchasedServerCostUpgrade(servers[i]);
+        const upgradeCost = player.money - ramUpgrade
+        let canUpgrade = false
+        if (upgradeCost > 0) {
+            canUpgrade = true
+        ns.tprint("Hostname: " + server.hostname, " MaxRAM: " + server.maxRam, "Total RAM : " + maxRAM, "Can Upgrade: " + canUpgrade);
+    }
     }
 }
